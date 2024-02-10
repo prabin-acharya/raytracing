704e9e1e126894c882869f597b4162787371cd5c
Prabin Acharya <acharyaprabin101@gmail.com>
2024-02-10 17:19:30 +0545

    added color.h to represent and log color

--- main.cc
+++ main.cc
@@ -1,15 +1,16 @@
 #include <iostream>
 
+#include "color.h"
+#include "vec3.h"
+
 int main()
 {
 
     // Image
-
     int image_width = 256;
     int image_height = 256;
 
     // Render
-
     std::cout << "P3\n"
               << image_width << ' ' << image_height << "\n255\n";
 
@@ -19,15 +20,8 @@ int main()
 
         for (int i = 0; i < image_width; ++i)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.                 \n";

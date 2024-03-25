bac0d3e9e47ab233d2d4769ec418437bc04a1e67
Prabin Acharya <acharyaprabin101@gmail.com>
2024-03-25 22:45:07 +0545

    back agin<3, hittable and sphere intersection logic

--- main.cc
+++ main.cc
@@ -4,32 +4,36 @@
 #include "vec3.h"
 #include "ray.h"
 
-double hit_sphere(const point3 &center, double radius, const ray &r)
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "rtweekend.h"
+
+// double hit_sphere(const point3 &center, double radius, const ray &r)
+// {
+//     vec3 oc = r.origin() - center;
+//     auto a = r.direction().length_squared();
+//     auto half_b = dot(oc, r.direction());
+//     auto c = oc.length_squared() - radius * radius;
+//     auto discriminant = half_b * half_b - a * c;
+
+//     if (discriminant < 0)
+//     {
+//         return -1.0;
+//     }
+//     else
+//     {
+
+//         return (-half_b - sqrt(discriminant)) / a;
+//     }
+// }
+
+color ray_color(const ray &r, const hittable &world)
 {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius * radius;
-    auto discriminant = half_b * half_b - a * c;
-
-    if (discriminant < 0)
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec))
     {
-        return -1.0;
-    }
-    else
-    {
-
-        return (-half_b - sqrt(discriminant)) / a;
-    }
-}
-
-color ray_color(const ray &r)
-{
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0)
-    {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -46,6 +50,12 @@ int main()
 
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    // World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto focal_length = 1.0;
@@ -77,7 +87,7 @@ int main()
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }

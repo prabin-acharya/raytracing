063d0c67fa2a57e3c7a3e6eab0fa0c434947efab
Prabin Acharya <acharyaprabin101@gmail.com>
2024-03-27 00:15:09 +0545

    use interval class for ray-object interaction

--- interval.h
+++ interval.h
@@ -1,6 +1,8 @@
 #ifndef INTERVAL_H
 #define INTERVAL_H
 
+#include "rtweekend.h"
+
 class interval
 {
 public:
--- rtweekend.h
+++ rtweekend.h
@@ -19,5 +19,6 @@ inline double degrees_to_radians(double degrees)
 
 #include "ray.h"
 #include "vec3.h"
+#include "interval.h"
 
 #endif

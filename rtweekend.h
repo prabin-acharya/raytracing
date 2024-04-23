328da2bd81a5841444ce3e912485a10ae6f90282
Prabin Acharya <acharyaprabin101@gmail.com>
2024-04-23 09:32:08 +0545

    generate random numer

--- rtweekend.h
+++ rtweekend.h
@@ -5,6 +5,7 @@
 #include <limits>
 #include <memory>
 #include <iostream>
+#include <cstdlib>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -16,6 +17,18 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+// [0, 1)
+inline double random_double()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// [min,max)
+inline double random_double(double min, double max)
+{
+    return min + (max - min) * random_double();
 }
 
 #include "ray.h"
